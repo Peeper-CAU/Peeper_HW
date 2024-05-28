91b78a6a00d1b459110dfac7bc9950c25edd0f63
yymin1022 <yymin1022@gmail.com>
2024-05-29 02:10:04 +0900

    Update WAV Util : Added WAV File Output Functions

--- main/wav_util.c
+++ main/wav_util.c
@@ -1,8 +1,6 @@
 #include "wav_util.h"
-#define PCM_BUFFER_SIZE 240
 
-int16_t pcm_data[PCM_BUFFER_SIZE];
-int pcm_data_size = 0;
+static void init_output_wav_header(FILE *file, int sample_rate, int num_channels, int bits_per_sample, int num_samples);
 
 void log_pcm_data(const int16_t *pcm_data, int num_samples) {
     for (int i = 0; i < num_samples; i++) {
@@ -12,4 +10,48 @@ void log_pcm_data(const int16_t *pcm_data, int num_samples) {
         }
     }
     printf("\n");
+}
+
+void close_output_wav_file() {
+    if (output_wav_file) {
+        fseek(output_wav_file, 0, SEEK_SET);
+        write_wav_header(output_wav_file, 16000, 1, 16, total_wav_samples);
+        fclose(output_wav_file);
+        output_wav_file = NULL;
+    }
+}
+
+void init_output_wav_file(const char *filename) {
+    output_wav_file = fopen(filename, "wb");
+    if (!output_wav_file) {
+        printf("Failed to open WAV file\n");
+        return;
+    }
+
+    init_output_wav_header(output_wav_file, 16000, 1, 16, 0);
+}
+
+static void init_output_wav_header(FILE *file, int sample_rate, int num_channels, int bits_per_sample, int num_samples) {
+    int byte_rate = sample_rate * num_channels * bits_per_sample / 8;
+    int block_align = num_channels * bits_per_sample / 8;
+    int subchunk2_size = num_samples * num_channels * bits_per_sample / 8;
+    int chunk_size = 36 + subchunk2_size;
+
+    fwrite("RIFF", 1, 4, file);
+    fwrite(&chunk_size, 4, 1, file);
+    fwrite("WAVE", 1, 4, file);
+    fwrite("fmt ", 1, 4, file);
+
+    int subchunk1_size = 16;
+    int audio_format = 1;
+    fwrite(&subchunk1_size, 4, 1, file);
+    fwrite(&audio_format, 2, 1, file);
+    fwrite(&num_channels, 2, 1, file);
+    fwrite(&sample_rate, 4, 1, file);
+    fwrite(&byte_rate, 4, 1, file);
+    fwrite(&block_align, 2, 1, file);
+    fwrite(&bits_per_sample, 2, 1, file);
+
+    fwrite("data", 1, 4, file);
+    fwrite(&subchunk2_size, 4, 1, file);
 }
--- main/wav_util.h
+++ main/wav_util.h
@@ -5,12 +5,14 @@
 # include <stdio.h>
 # define PCM_BUFFER_SIZE 240
 
+FILE	*output_wav_file;
 int16_t	pcm_data[PCM_BUFFER_SIZE];
 int		pcm_data_size = 0;
+int		total_wav_samples = 0;
 
 void	log_pcm_data(const int16_t *pcm_data, int num_samples);
 
-void	close_wav_file();
-void	init_wav_file(const char *filename);
-void	write_wav_header(FILE *file, int sample_rate, int num_channels, int bits_per_sample, int num_samples);
+void	close_output_wav_file();
+void	init_output_wav_file(const char *filename);
+
 #endif
