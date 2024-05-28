d20640230a996b9bf5da526c5df4ad58d5e4eb16
yymin1022 <yymin1022@gmail.com>
2024-05-29 02:07:02 +0900

    Update WAV Util : Added WAV File Output Functions

--- main/wav_util.h
+++ main/wav_util.h
@@ -1,7 +1,8 @@
 #ifndef WAV_UTIL_H
 # define WAV_UTIL_H
 
-# include "stdint.h"
+# include <stdint.h>
+# include <stdio.h>
 # define PCM_BUFFER_SIZE 240
 
 int16_t	pcm_data[PCM_BUFFER_SIZE];
@@ -9,4 +10,7 @@ int		pcm_data_size = 0;
 
 void	log_pcm_data(const int16_t *pcm_data, int num_samples);
 
+void	close_wav_file();
+void	init_wav_file(const char *filename);
+void	write_wav_header(FILE *file, int sample_rate, int num_channels, int bits_per_sample, int num_samples);
 #endif
