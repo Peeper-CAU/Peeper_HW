8c223a38f1da4831301b337dc761f3e89a6f2b5f
yymin1022 <yymin1022@gmail.com>
2024-06-11 03:30:18 +0900

    Update ble_util.c : Added Basic BLE Initialization Logic

--- main/ble_util.c
+++ main/ble_util.c
@@ -10,7 +10,7 @@ static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *p
                 printf("Advertising report received\n");
                 break;
             case HCI_EVENT_COMMAND_COMPLETE:
-                printf("Command complete\n");
+                // printf("Command complete\n");
                 break;
             case HCI_EVENT_LE_META:
                 switch (hci_event_le_meta_get_subevent_code(packet))
@@ -29,5 +29,15 @@ static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *p
 void	init_ble(void)
 {
 	printf("## Initializing BLE\n");
+	l2cap_init();
+	sm_init();
+
+    gap_advertisements_enable(1);
+
+	static btstack_packet_callback_registration_t hci_event_callback_registration;
+    hci_event_callback_registration.callback = &ble_packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+
+    hci_power_control(HCI_POWER_ON);
 	printf("## Initialized BLE\n");
 }
