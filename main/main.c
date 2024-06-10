d634c2ceed4049d51c4ba1601677b692b9a36119
yymin1022 <yymin1022@gmail.com>
2024-06-11 03:17:11 +0900

    Update main.c : Removed unnecessary BT Controller Initialization

--- main/main.c
+++ main/main.c
@@ -14,12 +14,6 @@ int app_main(void)
 	nvs_flash_erase();
 	nvs_flash_init();
 
-	esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-    esp_bt_controller_init(&bt_cfg);
-    esp_bt_controller_enable(ESP_BT_MODE_BTDM);
-    esp_bluedroid_init();
-    esp_bluedroid_enable();
-
     btstack_init();
 
     btstack_audio_source_set_instance(btstack_audio_esp32_source_get_instance());
