719a6a279d26d7c7cd5ebe686643752feaae1c3c
yymin1022 <yymin1022@gmail.com>
2024-06-10 18:47:58 +0900

    Update ble_util.c : Added Basic Functions

--- main/ble_util.c
+++ main/ble_util.c
@@ -1,11 +1,5 @@
 #include "ble_util.h"
 
-static const uint8_t adv_data[] = {
-    0x02, 0x01, 0x06,
-    0x03, 0x03, 0xAB, 0xCD,
-    0x0B, 0x09, 'P', 'E', 'E', 'P', 'E', 'R', '_', 'B', 'L', 'E'
-};
-
 static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
 {
     if (packet_type == HCI_EVENT_PACKET)
@@ -34,10 +28,6 @@ static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *p
 
 void	init_ble(void)
 {
-    l2cap_init();
-    sm_init();
-    gap_advertisements_set_params(0x0030, 0x0030, 0x00, 0x00, NULL, 0x07, 0x00);
-    gap_advertisements_set_data(sizeof(adv_data), (uint8_t*)adv_data);
-    gap_advertisements_enable(1);
-	btstack_run_loop_execute();
+	printf("## Initializing BLE\n");
+	printf("## Initialized BLE\n");
 }
