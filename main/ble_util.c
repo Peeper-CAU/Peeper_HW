38411e08848282f6d50c0c418e69a95e66553d45
yymin1022 <yymin1022@gmail.com>
2024-06-12 05:33:28 +0900

    Update ble_util.c : Added Logic for BLE Data Sending

--- main/ble_util.c
+++ main/ble_util.c
@@ -1,5 +1,11 @@
 #include "ble_util.h"
 
+static uint8_t service_uuid[16];
+static uint8_t characteristic_uuid[16];
+
+static uint16_t connection_handle = HCI_CON_HANDLE_INVALID;
+static uint16_t att_handle = 0;
+
 static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
 {
     if (packet_type == HCI_EVENT_PACKET)
@@ -10,6 +16,7 @@ static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *p
                 switch (hci_event_le_meta_get_subevent_code(packet))
                 {
                     case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
+                        connection_handle = hci_subevent_le_connection_complete_get_connection_handle(packet);
                         printf("LE Connection complete\n");
                         break;
                 }
@@ -20,11 +27,26 @@ static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *p
     }
 }
 
+void ble_send_data(uint8_t *data, uint16_t length)
+{
+    if (connection_handle != HCI_CON_HANDLE_INVALID && att_handle != 0)
+    {
+        att_server_notify(connection_handle, att_handle, data, length);
+    }
+}
+
 void	init_ble(void)
 {
 	printf("## Initializing BLE\n");
 	l2cap_init();
 	sm_init();
+
+	att_db_util_init();
+    att_db_util_add_service_uuid128(service_uuid);
+    att_handle = att_db_util_add_characteristic_uuid128(characteristic_uuid, ATT_PROPERTY_NOTIFY, ATT_SECURITY_NONE, ATT_SECURITY_NONE, NULL, NULL);
+
+    att_server_init(att_db_util_get_address(), NULL, NULL);
+    att_server_register_packet_handler(ble_packet_handler);
 
 	uint16_t adv_interval_min = 0x0020;
     uint16_t adv_interval_max = 0x0030;
