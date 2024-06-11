86b8fb480159b9d8040979e1550cd3892f95825c
yymin1022 <yymin1022@gmail.com>
2024-06-12 05:23:15 +0900

    Update ble_util.c : Removed Unnecessary Packet Handling

--- main/ble_util.c
+++ main/ble_util.c
@@ -5,16 +5,10 @@ static void	ble_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *p
     if (packet_type == HCI_EVENT_PACKET)
 	{
         switch (hci_event_packet_get_type(packet))
-		{
-            case GAP_EVENT_ADVERTISING_REPORT:
-                printf("Advertising report received\n");
-                break;
-            case HCI_EVENT_COMMAND_COMPLETE:
-                // printf("Command complete\n");
-                break;
+        {
             case HCI_EVENT_LE_META:
                 switch (hci_event_le_meta_get_subevent_code(packet))
-				{
+                {
                     case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
                         printf("LE Connection complete\n");
                         break;
