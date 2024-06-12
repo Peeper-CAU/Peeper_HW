9ea646195e62c4a3f5bd7bf226fd8365d9c250f7
yymin1022 <yymin1022@gmail.com>
2024-06-13 04:46:27 +0900

    Update processing_util.h : Added Including of PCM Util

--- main/processing_util.h
+++ main/processing_util.h
@@ -1,13 +1,16 @@
 #ifndef PROCESSING_UTIL_H
 # define PROCESSING_UTIL_H
 
+# define SCO_CVSD_PA_PREBUFFER_MS 50
 # define SCO_MSBC_PA_PREBUFFER_MS 50
 # define SCO_REPORT_PERIOD 100
 
 # define NUM_CHANNELS 1
+# define CVSD_SAMPLE_RATE 8000
 # define MSBC_SAMPLE_RATE 16000
 # define BYTES_PER_FRAME 2
 
+# define CVSD_PA_PREBUFFER_BYTES (SCO_CVSD_PA_PREBUFFER_MS * CVSD_SAMPLE_RATE / 1000 * BYTES_PER_FRAME)
 # define MSBC_PA_PREBUFFER_BYTES (SCO_MSBC_PA_PREBUFFER_MS * MSBC_SAMPLE_RATE / 1000 * BYTES_PER_FRAME)
 
 # define MAX_NUM_MSBC_SAMPLES (16 * 8)
@@ -16,10 +19,13 @@
 # include "btstack_audio.h"
 # include "btstack_debug.h"
 # include "btstack_ring_buffer.h"
+# include "classic/btstack_cvsd_plc.h"
 # include "classic/btstack_sbc.h"
 # include "classic/hfp.h"
 # include "classic/hfp_msbc.h"
 # include "hci.h"
+
+# include "pcm_util.h"
 
 void	processing_close(void);
 void	processing_init(void);
