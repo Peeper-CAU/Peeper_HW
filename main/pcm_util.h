be87d8e8b765622f7f4db847f24d1921313e8684
yymin1022 <yymin1022@gmail.com>
2024-06-13 04:47:54 +0900

    Update pcm_util.h : Added PCM Data Sending Function

--- main/pcm_util.h
+++ main/pcm_util.h
@@ -1,10 +1,11 @@
 #ifndef BLE_PCM_UTIL_H
 # define BLE_PCM_UTIL_H
 
-// # include <stddef.h>
 # include <stdint.h>
 # include <stdio.h>
 # include <string.h>
+
+# include "ble_util.h"
 
 # define PCM_BUFFER_SIZE 240
 
@@ -12,5 +13,6 @@ extern int16_t	pcm_data[PCM_BUFFER_SIZE];
 extern int		pcm_data_size;
 
 void	log_pcm_data(const int16_t *pcm_data, int num_samples);
+void	send_pcm_data(const int16_t *pcm_data, int num_samples);
 
 #endif
