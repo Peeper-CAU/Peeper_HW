a3d7077687ddfd15072afc0916a863c62754a550
yymin1022 <yymin1022@gmail.com>
2024-06-12 21:20:55 +0900

    Update main.c : Removed unnecessary nvs flash init

--- main/main.c
+++ main/main.c
@@ -1,7 +1,6 @@
 #include "btstack_audio.h"
 #include "btstack_port_esp32.h"
 #include "btstack_run_loop.h"
-#include "nvs_flash.h"
 
 #include "ble_util.h"
 #include "hfp_util.h"
@@ -9,9 +8,6 @@
 
 int app_main(void)
 {
-	nvs_flash_erase();
-	nvs_flash_init();
-
     btstack_init();
 
     btstack_audio_source_set_instance(btstack_audio_esp32_source_get_instance());
