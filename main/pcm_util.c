6a27acc139364954605ace72cfe3e7f50433b12e
yymin1022 <yymin1022@gmail.com>
2024-06-13 04:48:49 +0900

    Update pcm_util.h : Cleaned UP

--- main/pcm_util.c
+++ main/pcm_util.c
@@ -1,13 +1,16 @@
 #include "pcm_util.h"
 
-int16_t	pcm_data[PCM_BUFFER_SIZE];
-int		pcm_data_size = 0;
-
-void log_pcm_data(const int16_t *pcm_data, int num_samples) {
+void	log_pcm_data(const int16_t *pcm_data, int num_samples)
+{
 	printf("(Size %d) : ", num_samples);
 	int log_size = num_samples < 10 ? num_samples : 10;
     for(int i = 0; i < log_size; i++) {
         printf("%d ", pcm_data[i]);
     }
     printf("\n");
+}
+
+void	send_pcm_Data(const int16_t *pcm_data, int num_samples)
+{
+
 }
--- main/pcm_util.h
+++ main/pcm_util.h
@@ -7,11 +7,6 @@
 
 # include "ble_util.h"
 
-# define PCM_BUFFER_SIZE 240
-
-extern int16_t	pcm_data[PCM_BUFFER_SIZE];
-extern int		pcm_data_size;
-
 void	log_pcm_data(const int16_t *pcm_data, int num_samples);
 void	send_pcm_data(const int16_t *pcm_data, int num_samples);
 
