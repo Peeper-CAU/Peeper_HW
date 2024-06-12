8ed9c145292603a911cd4f69e1dec6ab2055e4dd
yymin1022 <yymin1022@gmail.com>
2024-06-13 04:46:00 +0900

    Update pcm_util.c : Fixed PCM Logging Format

--- main/pcm_util.c
+++ main/pcm_util.c
@@ -4,12 +4,10 @@ int16_t	pcm_data[PCM_BUFFER_SIZE];
 int		pcm_data_size = 0;
 
 void log_pcm_data(const int16_t *pcm_data, int num_samples) {
-	printf("%d (Size %d)", pcm_data[0], pcm_data_size);
-    // for (int i = 0; i < num_samples; i++) {
-    //     printf("%d ", pcm_data[i]);
-    //     if (i % 20 == 19) {
-    //         printf("\n");
-    //     }
-    // }
+	printf("(Size %d) : ", num_samples);
+	int log_size = num_samples < 10 ? num_samples : 10;
+    for(int i = 0; i < log_size; i++) {
+        printf("%d ", pcm_data[i]);
+    }
     printf("\n");
 }
